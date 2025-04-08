d44041783efc0e852575071599a999ff2e131358
ColeUyematsu <cjuyematsu@gmail.com>
2025-04-07 19:33:25 -0700

    update logo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Head from "next/head"
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>GitTempo</title>
-        <link rel="icon" type="image/png" href="/logo.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Visualize commit activity from GitHub repos in the last 72 hours." />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
     </>
